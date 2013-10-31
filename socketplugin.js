4477e49b382d9abc4d96b84802835ef0944c6e77
storage <storage@storage-desktop-4.(none)>
2013-10-31 17:30:14 +0800

    Init socket io

--- socketplugin.js
+++ socketplugin.js
@@ -1,9 +1,55 @@
 // # Socket Plugin Module
 
+const KEY = 'express.sid'
+	, SECRET = 'express';
 // Module dependencies
-var when        = require('when'),
-    express     = require('express'),
-    errors      = require('./server/errorHandling'),
-    fs          = require('fs'),
-    path        = require('path'),
-    plugins     = require('./server/plugins');
+
+var express = require('express')
+  , ghostPlugin = require('../../core/server/plugins/GhostPlugin')
+  , app = express()
+  , server = require('http').createServer(app)
+  , cookie = express.cookieParser(SECRET)
+  , store = new express.session.MemoryStore()
+  , session = express.session({secret: SECRET
+                             , key: KEY
+                             , store: store})
+  , io;
+
+ghostPlugin.install = function (ghost) {
+	var server = ghost.server;
+	server.configure(function(){
+	  server.use(cookie);
+	  server.use(session);
+	});
+	io = require('socket.io').listen(server);
+    return;
+};
+
+io.set('authorization', function(data, accept) {
+  cookie(data, {}, function(err) {
+    if (!err) {
+      var sessionID = data.signedCookies[KEY];
+      store.get(sessionID, function(err, session) {
+        if (err || !session) {
+          accept(null, false);
+        } else {
+          data.session = session;
+          accept(null, true);
+        }
+      });
+    } else {
+      accept(null, false);
+    }
+  });
+});
+
+io.sockets.on('connection', function (client) {
+  var session = client.handshake.session
+    , nome = session.nome;
+//nome = 'Test';
+  client.on('toServer', function (msg) {
+    msg = "<b>"+nome+":</b> "+msg+"<br>";
+    client.emit('toClient', msg);
+    client.broadcast.emit('toClient', msg);
+  });
+});
