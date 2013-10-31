89e753d9c9960bbaf4db1e7d323bdacbfc2c6474
storage <storage@storage-desktop-4.(none)>
2013-10-31 16:52:51 +0800

    New Init

---
+++ socketplugin.js
@@ -0,0 +1,8 @@
+// # Socket Plugin Module
+
+// Module dependencies
+var when        = require('when'),
+    express     = require('express'),
+    errors      = require('./server/errorHandling'),
+    fs          = require('fs'),
+    path        = require('path');
